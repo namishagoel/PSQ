55b0fcdd7942b49a665eb5c10eb1172140b67425
daxoron <daxoron.dev@gmail.com>
2021-09-09 09:15:07 +0530

    addedOneQuestion

---
+++ Questions/1_Code.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+#define test long long int t;cin >> t;while (t--)
+using namespace std;
+int main() {
+    //InputOutput
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    //Testcases
+    test {
+
+    }
+    return 0;
+}
