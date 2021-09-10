387430f2cd0f628760840f35fbcaa535fb0779f1
daxoron <daxoron.dev@gmail.com>
2021-09-10 19:30:19 +0530

    justChecking

--- Questions/1_Code.cpp
+++ Questions/1_Code.cpp
@@ -2,13 +2,15 @@
 
 #define test long long int t;cin >> t;while (t--)
 using namespace std;
-int main() {
+int main()
+{
     //InputOutput
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     //Testcases
-    test {
-
+    test
+    {
+        cout<<"Just Trying To Make My Git Green !!!";
     }
     return 0;
 }
