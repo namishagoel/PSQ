4f5d5d82f692a4225e7f77781ceb3018d50e5cbd
daxoron <daxoron.dev@gmail.com>
2021-09-11 18:28:38 +0530

    Added data structures folder

---
+++ DataStructures/Array.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    //Declaration
+    int arr[5],i;
+    //Input
+    for(i=0; i<5; i++)
+        cin>>arr[i];
+    //Output
+    for(i=0; i<5; i++)
+        cout<<arr[i]<<" ";
+}
---
+++ DataStructures/LinkedList.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+// Linked List is a linear data structure.
+// Multiple Blocks Of Memory Linked Together
+int main()
+{
+    //Linked_List
+
+}
