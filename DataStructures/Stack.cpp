08bdd3d166872d29260ee90fcb7b87758989fb3d
daxoron <daxoron.dev@gmail.com>
2021-09-21 12:08:12 +0530

    Added Stack Data Structure

--- DataStructures/Stack.cpp
+++ DataStructures/Stack.cpp
@@ -4,14 +4,91 @@ using namespace std;
 // We Can Only add or remove element from one end of the stack.
 // LIFO Mechanism -> Last in First out
 // Example Could Be : Pile of Coins or Pile of Plates.
-int main()
-{
-    
-    return 0;
-}
-//  Array Implementation of Stack
+// top = -1 // No Elements in the Stack
+// top = lastValueIndex // Stack is Full
+//  Array Implementation of stack
 //  push(val);
 //  pop();
 //  top();
 //  empty();
 //  Time Complexity O(1)
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define MAX 1000
+
+class Stack {
+	int top;
+
+public:
+	int a[MAX];
+	Stack()
+    { 
+        top = -1;
+    }
+	bool push
+    (
+        int x
+    );
+	int pop();
+	int peek();
+	bool isEmpty();
+};
+
+bool Stack::push(int x)
+{
+	if (top >= (MAX - 1)) {
+		cout << "Stack Overflow";
+		return false;
+	}
+	else {
+		a[++top] = x;
+		cout << x << " pushed into stack\n";
+		return true;
+	}
+}
+
+int Stack::pop()
+{
+	if (top < 0) {
+		cout << "Stack Underflow";
+		return 0;
+	}
+	else {
+		int x = a[top--];
+		return x;
+	}
+}
+int Stack::peek()
+{
+	if (top < 0) {
+		cout << "Stack is Empty";
+		return 0;
+	}
+	else {
+		int x = a[top];
+		return x;
+	}
+}
+
+bool Stack::isEmpty()
+{
+	return (top < 0);
+}
+
+int main()
+{
+	class Stack s;
+	s.push(1);
+	s.push(2);
+	s.push(3);
+	cout << s.pop() << " Popped from stack\n";
+	cout<<"Elements present in stack : ";
+	while(!s.isEmpty())
+	{
+		cout<<s.peek()<<" ";
+		s.pop();
+	}
+	return 0;
+}
