473856ca4e00370eae32c17a48b8e0a93962235e
daxoron <daxoron.dev@gmail.com>
2021-09-30 19:09:47 +0530

    Updated template.cpp

--- Questions/LargestSubarraySum_Brute.cpp
+++ Questions/LargestSubarraySum_Brute.cpp
@@ -1,7 +1,31 @@
 #include<bits/stdc++.h>
 
 using namespace std;
-int largest(int arr[], int n)
+
+// O(N^2)
+int largest2(int arr[],int n)
+{
+    //PrefixSumArrayCalculated
+    int prefix [n] = {0},large = INT_MIN;
+    prefix[0] = arr[0];
+    for(int i = 1; i<n; i++)
+        prefix[i] = arr[i] + prefix[i-1];
+    //LargestSumSubarray
+    for(int i = 0; i<n; i++)
+    {
+        for(int j = i; j<n; j++)
+        {
+            int sum = 0;
+            sum+= i>0 ? prefix[j] - prefix[i-1]:prefix[j];
+            large = max(large,sum);
+        }
+    }
+
+    return large;
+}
+
+// O(N^3)
+int largest3(int arr[], int n)
 {
     int large = INT_MIN;
     for(int i = 0; i<n; i++)
@@ -18,9 +42,12 @@ int largest(int arr[], int n)
     }
     return large;
 }
+
 int main()
 {
-    int arr[6] = {1,-1,6,3,1,-7},n=6;
-    cout<<"\n\n"<<largest(arr,n);
+    int arr[6] = {1,1,-6,3,1,-7},n=6;
+    cout<<"\n\n"<<largest3(arr,n);
+    cout<<"\n\n"<<largest2(arr,n);
+    cout<<"\n\n"<<largest1(arr,n);
     return 0;
 }
