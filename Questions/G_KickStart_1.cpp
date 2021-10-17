b62123367bfe79ef3269ec30bcf7155d96f4f825
daxoron <daxoron.dev@gmail.com>
2021-10-17 15:53:15 +0530

    Google Kickstart Round G

--- Questions/G_KickStart_1.cpp
+++ Questions/G_KickStart_1.cpp
@@ -30,39 +30,57 @@ int main(){
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   //Testcases
+  int f = 1;
   test
   {
       long long int n,d,c,m,i,count_d=0;
       cin>>n>>d>>c>>m;
       string st;
       cin>>st;
+
       for(i = 0; i<n; i++)
       {
           if(st[i]=='D')
-          {
             count_d++;
-          }
       }
+
       int flag = 1;
+
       for(i = 0; i<n; i++)
       {
-        if(flag==-1)
-          break;
+
         if(st[i]=='D')
         {
           if(d==0)
             flag=-1;
-          else if(d>0)
-          {
+          else{  
             d--;
             c+=m;
             count_d--;
-          }
+            }
         }
         else if(st[i]=='C')
         {
-          c--;
+          if(c==0)
+            flag=-1;
+          else
+          {
+              c--;
+          }
         }
+        if(flag==-1)
+          break;
+      }
+
+      if(count_d==0)
+      {
+        cout<<"Case #"<<f<<": YES\n";
+        f++;
+      }
+      else
+      {
+        cout<<"Case #"<<f<<": NO\n";
+        f++;
       }
   }
   return 0;
